eaacc9b178be578c86f6ec4ac0688541cd5b2918
ninobonanno <nino.bonanno96@gmail.com>
2020-09-27 16:19:08 +0200

    update street

--- python/strada/Street.py
+++ python/strada/Street.py
@@ -46,18 +46,19 @@ class Street:
         self.__available = DB_Street.available
 
         self.__threadCount = 0
-        self.__lenght = lenght
+        self.__lenght = lenght  # metri
         self.__maxSpeed = maxSpeed
-        self.__signals = self.__createSignals(signals_quantity, 5, 5, 10)
+        self.__signals = self.__createSignals(signals_quantity, 20, 5)
 
-    def __createSignals(self, signals_quantity: list, step: int, stop_dist: int, time_semaphore: int):
+    def __createSignals(self, signals_quantity: list, step: int, time_semaphore: int):
         street_signal = list()
+        stop = segnali.Stop()
 
         for i in signals_quantity:  # tuple (nome segnale, quantità)
             for count in range(i[1]):
                 while True:
                     position = randrange(5, self.__lenght, step)
-                    if(position not in (j[1] for j in street_signal) and position < (self.__lenght - stop_dist)):
+                    if(position not in (j[1] for j in street_signal) and position < (self.__lenght - stop.getDelta())):
                         break
 
                 if (i[0] == "speed_limit"):
@@ -72,11 +73,15 @@ class Street:
                     "Il segnale ", street_signal[-1][0].getName(), "è nella posizione ", position)
 
         # stop fine strada
-        street_signal.append((segnali.Stop(), self.__lenght))
+        street_signal.append((stop, self.__lenght))
         return street_signal
 
     def __findSignal(self, client_position: float):
         # signal[0] è il segnale, signal[1] è la sua posizione nella strada
+        if client_position >= self.__lenght:
+            stop = self.__signals[-1]
+            return stop[0], stop[1]
+
         for signal in self.__signals:
             if ((signal[1] - client_position < signal[0].delta) and (signal[1] - client_position > 0)):
                 return signal[0], signal[1]
@@ -148,6 +153,7 @@ class Street:
         old_position = DB_Route.current_street_position
         new_position = ((client_speed / 3.6) * new_date_time) + old_position
         if client_position is not None and new_position != client_position:
+            print(client_position)
             # il client si trova in una posizione diversa, teoricamente il confronto delle posizioni deve sempre coincidere
             new_position = client_position
 
@@ -166,7 +172,7 @@ class Street:
             "distance": signal_position - new_position,
             "speed_limit": signal.getSpeed() if name_signal == "speed_limit" else None
         }
-        return new_position, action, f"Fra {action['distance']} m incontri il segnale {name_signal}, l'azione che devi eseguire è {action['action']}"
+        return new_position, action, f"Fra {action['distance']:.2f}m incontri il segnale {name_signal}, l'azione che devi eseguire e' {action['action']}"
 
     @threaded
     def __manageCar(self, client, client_address):
@@ -226,15 +232,17 @@ class Street:
 
         self.__s.close()
 
+
 def arg_tuple_parse(arg_list):
-    list_signal=[]
+    list_signal = []
     for i in arg_list:
-        field_args=i.split(',')
-        if(len(field_args)==2):
-            list_signal.append(tuple((field_args[0],int(field_args[1]))))
+        field_args = i.split(',')
+        if(len(field_args) == 2):
+            list_signal.append(tuple((field_args[0], int(field_args[1]))))
         else:
             raise Exception("Errore formattazione dati")
     print(list_signal)
+
 
 if __name__ == '__main__':
 
@@ -244,11 +252,11 @@ if __name__ == '__main__':
     parser.add_argument('-l', '--st-lenght', type=int, default=50)
     parser.add_argument('-s', '--speed', type=int, default=50)
     parser.add_argument('-n', '--name', type=str, default="road1")
-    parser.add_argument('-st', '--sig-type', nargs='+', type=str) #-st stop,2 speed_limit,2
+    parser.add_argument('-st', '--sig-type', nargs='+',
+                        type=str)  # -st stop,2 speed_limit,2
     args = parser.parse_args()
 
-
-    #sig_type = [('semaphore', 2), ('speed_limit', 3)]  # args.sig_type
+    # sig_type = [('semaphore', 2), ('speed_limit', 3)]  # args.sig_type
 
     street = Street(args.name, args.speed, args.st_lenght,
                     arg_tuple_parse(args.sig_type), args.ip_address, args.port)
